68e7111e20c6a6a10728e3db2428d131db74f73f
Amansaif76 <amansaif7691@gmail.com>
2024-08-14 16:17:26 +0530

    Add files via upload

---
+++ aarr max and min.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int n;
+	printf("enter the size of array: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter the values of array: \n");
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("array elemenrts are: \n");
+	for(int i=0;i<n;i++){
+		printf("%d \n",arr[i]);
+	}
+	int max = arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]>max)
+		max = arr[i];
+	}
+	int min = arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]<min)
+		min = arr[i];
+	}
+	printf("min  and max value is %d %d",min ,max);
+	return 0;
+}
